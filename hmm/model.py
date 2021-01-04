2746bf2a4955f8e124fa7ac349e4a3b609cbb4ee
Piotr Mikolajczyk <piomiko41@gmail.com>
2021-01-04 20:45:12 +0100

    Rendering model

--- hmm/model.py
+++ hmm/model.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from graphviz import Digraph
+
 from hmm.state import State, default_distribution
 
 
@@ -20,3 +22,19 @@ class Model:
             state.name = id
         self._adj = [[(n.name, tp) for n, tp in zip(s.neigh, s.trans)]
                      for s in states]
+
+    def render(self) -> None:
+        dot = Digraph(graph_attr={'rankdir': 'LR'}, node_attr={'shape': 'circle'})
+        n = len(self._states)
+        for s in self._states:
+            if not s.is_emitting:
+                dot.node(str(s.name), label='', width='0.2', style='filled')
+        for u in range(n):
+            for v, puv in self._adj[u]:
+                dot.edge(str(u), str(v), label=str(puv))
+        dot.render('model.gv', view=True)
+
+
+if __name__ == '__main__':
+    m = Model.Path(3)
+    m.render()
